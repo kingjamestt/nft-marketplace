815db629d79942483bb61c492dfb724c1c09c8e4
Brandon James <brandonjames101@gmail.com>
2022-06-07 16:15:12 -0400

    refactored styles

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { COLORS, SIZES, SHADOWS, assets } from '../constants'
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
+    const navigation = useNavigation()
+
     return (
-        <View>
-            <Text>NFTCard</Text>
+        <View style={styles.card}>
+            <Text>NFT Card</Text>
         </View>
     )
 }
 
 export default NFTCard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base
+    },
+})
--- screens/Home.js
+++ screens/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
     //Loading Animation
     if (loading) return (
-        <View style={styles.container}>
+        <View style={styles.loading}>
             <Text>
                 Loading...
             </Text>
@@ -24,26 +24,19 @@ const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <FocusedStatusBar background={COLORS.primary} />
-            <View style={{ flex: 1 }}>
-                <View style={{ zIndex: 0 }}>
+            <View style={styles.flexContainer}>
+                <View style={styles.flatListContainer}>
                     <FlatList
                         data={NFTData}
-                        renderItem={({ item }) => <Text>{item.name}</Text>}
+                        renderItem={({ item }) => <NFTCard data={item} />}
                         keyExtractor={(item) => item.id}
                         showsVerticalScrollIndicator={false}
                         ListHeaderComponent={HomeHeader}
                     />
                 </View>
-                <View style={{
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    zIndex: -1,
-                }}>
-                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
-                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+                <View style={styles.backgroundContainer}>
+                    <View style={styles.heroBg} />
+                    <View style={styles.contentBg} />
                 </View>
             </View>
         </SafeAreaView>
@@ -53,9 +46,31 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-    container: {
+    loading: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    flexContainer: {
+        flex: 1
+    },
+    flatListContainer: {
+        zIndex: 0
+    },
+    backgroundContainer: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: -1,
+    },
+    heroBg: {
+        height: 300,
+        backgroundColor: COLORS.primary
+    },
+    contentBg: {
+        flex: 1,
+        backgroundColor: COLORS.white
     },
 })
