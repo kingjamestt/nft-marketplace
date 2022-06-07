0264200877fd86eb7daa025be54aeee0c38b93db
Brandon James <brandonjames101@gmail.com>
2022-06-07 15:41:57 -0400

    Home screen layout

--- screens/Home.js
+++ screens/Home.js
@@ -9,11 +9,10 @@ const Home = () => {
     useEffect(() => {
         //do something before loading
         // ...
-        // ...
-        // ...
         setLoading(false)
     }, [])
 
+    //Loading Animation
     if (loading) return (
         <View style={styles.container}>
             <Text>
@@ -23,8 +22,30 @@ const Home = () => {
     )
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Home</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <FocusedStatusBar background={COLORS.primary} />
+            <View style={{ flex: 1 }}>
+                <View style={{ zIndex: 0 }}>
+                    <FlatList
+                        data={NFTData}
+                        renderItem={({ item }) => <Text>{item.name}</Text>}
+                        keyExtractor={(item) => item.id}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={HomeHeader}
+                    />
+                </View>
+                <View style={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    zIndex: -1,
+                }}>
+                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
@@ -33,9 +54,8 @@ export default Home
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
 })
