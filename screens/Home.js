2115ba49b4908dab8d838e7632451bf002c97a04
Brandon James <brandonjames101@gmail.com>
2022-06-07 14:28:26 -0400

    assets and constants added

---
+++ screens/Home.js
@@ -0,0 +1,40 @@
+import { SafeAreaView, StyleSheet, Text, FlatList, View } from 'react-native'
+import { useState, useEffect } from 'react'
+import { COLORS, NFTData } from '../constants'
+
+const Home = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        //do something before loading
+        // ...
+        // ...
+        // ...
+        setLoading(false)
+    }, [])
+
+    if (loading) return (
+        <View style={styles.container}>
+            <Text>
+                Loading...
+            </Text>
+        </View>
+    )
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
